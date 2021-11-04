a3274c223a4d4b32c238e50e0013fb87bf5e1e7c
Storm <87811650+Storm99999@users.noreply.github.com>
2021-11-04 11:18:02 +0100

    Add files via upload

---
+++ Examples/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using SyntaxSharp.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program : GeneralSyntaxScript
+    { 
+        static void Main(string[] args)
+        {
+            print("test");
+            if (getKey(ConsoleKey.C) == true)
+            {
+                print("Cool!");
+            }
+            pause();
+        }
+
+        public static string str()
+        {
+            // ret is a return function currently being worked on
+            ret("");
+            return "N/A";
+        }
+    }
+}
